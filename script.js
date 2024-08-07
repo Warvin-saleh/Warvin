4e4759b76cb9f5e83a0d4d84b366da927c507574
Warvin-saleh <kawaduhok30@gmail.com>
2024-08-08 02:09:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { // Adjust this value as needed
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+
